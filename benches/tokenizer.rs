097328f5890a3c98fdecbc51b39643c04a65288e
aleics <aleics.casanovas@gmail.com>
2020-03-22 21:01:15 +0100

    Include bench tests for StreamTokenizer to compare with StringTokenizer

--- benches/tokenizer.rs
+++ benches/tokenizer.rs
@@ -4,17 +4,33 @@ extern crate test;
 
 use test::Bencher;
 
-use strizer::{StringTokenizer, Token};
+use std::io::Cursor;
+use strizer::{StreamTokenizer, StringTokenizer, Token};
 
 #[bench]
-fn bench_tokenize_words(b: &mut Bencher) {
-  let tokenizer = StringTokenizer::new("hello world");
+fn bench_string_tokenize_words(b: &mut Bencher) {
+  b.iter(|| StringTokenizer::new("hello world").collect::<Vec<Token>>());
+}
+
+#[bench]
+fn bench_string_tokenize_ordinary_chars(b: &mut Bencher) {
+  let mut tokenizer = StringTokenizer::new("hello world");
+  tokenizer.ordinary_char('o');
   b.iter(|| tokenizer.clone().collect::<Vec<Token>>());
 }
 
 #[bench]
-fn bench_tokenize_ordinary_chars(b: &mut Bencher) {
-  let mut tokenizer = StringTokenizer::new("hello world");
-  tokenizer.ordinary_char('o');
-  b.iter(|| tokenizer.clone().collect::<Vec<Token>>());
+fn bench_stream_tokenize_words(b: &mut Bencher) {
+  let cursor = &mut Cursor::new("hello\nworld!\n");
+  b.iter(|| StreamTokenizer::new(cursor).collect::<Vec<Token>>());
+}
+
+#[bench]
+fn bench_stream_tokenize_ordinary_chars(b: &mut Bencher) {
+  let cursor = &mut Cursor::new("hello\nworld!\n");
+  b.iter(|| {
+    let mut tokenizer = StreamTokenizer::new(cursor);
+    tokenizer.ordinary_char('o');
+    tokenizer.collect::<Vec<Token>>()
+  });
 }
